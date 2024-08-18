56923196714977150dd76533bffd3a4b3b7ada41
chirag_k_poojary <chiragkpoojary30@gmail.com>
2024-08-18 13:15:18 +0530

    cors handled

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-#![recursion_limit = "256"]
-use std::fs;
-
+use actix_cors::Cors;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-
+use std::fs;
 
 #[get("/")]
 async fn jokes() -> impl Responder {
-    
     let data = fs::read_to_string("src/jokes.json").unwrap();
-    
-    
     let json_data: serde_json::Value = serde_json::from_str(&data).expect("file loading failed");
-    
-    
     HttpResponse::Ok().json(json_data)
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
-    HttpServer::new(||{
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
         App::new()
-        .service(jokes)})
+            .wrap(
+                Cors::default()
+                    .allow_any_origin() // Allow all origins
+                    .allow_any_method() // Allow all methods
+                    .allow_any_header() // Allow all headers
+                    .supports_credentials() // Support cookies
+            )
+            .service(jokes)
+    })
     .bind("0.0.0.0:3000")?
     .run()
     .await
-
 }
